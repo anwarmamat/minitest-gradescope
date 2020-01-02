10cd75dfb52eef37d185dddb72b947081ebb3d7a
Anwar Mamat <anwarmamat@gmail.com>
2020-01-02 11:11:34 -0500

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 apt-get -y install ruby
 
-cd /autograder/source/minitest-gradescope
+cd /autograder/source/minitest-gradescope-plugin
 gem install gradescope
 cd /
-
-# The following lines are to make sure we can install packages from maven
-# See https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
-# and https://github.com/docker-library/openjdk/issues/19#issuecomment-70546872
-# apt-get install --reinstall ca-certificates-java
-# update-ca-certificates -f
